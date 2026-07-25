915eb9fcbc3df029f5bf0e2c2c6b4cbe83c877e1
mibienpanjoe <epixprint00@gmail.com>
2026-07-25 01:32:07 +0000

    get tracker url

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf8'));
-//console.log(torrent);
+
+const bencode = require('bencode');
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+console.log(torrent.announce.toString('utf8'));
