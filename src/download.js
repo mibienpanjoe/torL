bafaf7267812e171b8644f21d157d6514b2ff530
mibienpanjoe <epixprint00@gmail.com>
2026-07-25 20:10:56 +0000

    feat: add handshake handler

--- src/download.js
+++ src/download.js
@@ -3,22 +3,35 @@
 const net = require('net');
 const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const message = require('./message');
 
 module.exports = torrent => {
   tracker.getPeers(torrent, peers => {
-    peers.forEach(download);
+    // 1
+    peers.forEach(peer => download(peer, torrent));
   });
 };
 
-function download(peer) {
-  const socket = net.Socket();
+function download(peer, torrent) {
+  const socket = new net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
-    // socket.write(...) write a message here
+    // 1
+    socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsg(socket, data => {
-    // handle response here
-  });
+  // 2
+  onWholeMsg(socket, msg => msgHandler(msg, socket));
+}
+
+// 2
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+// 3
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 &&
+         msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
 
 function onWholeMsg(socket, callback) {
